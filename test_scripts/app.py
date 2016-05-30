6d2a3285d0e4000e94b10faa884cde0946199784
Ramon Melo <me@ramonmelo.com>
2016-05-30 11:41:15 -0400

    update test file

--- test_scripts/app.py
+++ test_scripts/app.py
@@ -10,7 +10,12 @@ if len(sys.argv) > 1:
 else:
     port = 5760
 
+if len(sys.argv) > 2:
+    idx = int(sys.argv[2])
+else:
+    idx = 0
+
 url = 'tcp:127.0.0.1:%d' % (port)
 
-vehicle = DroidManager.connect_drone(url, source_system = 200)
+vehicle = DroidManager.connect_drone(url, idx, source_system = 200)
 vehicle.run()
